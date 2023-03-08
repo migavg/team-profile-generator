cdb300bf105aef39ec3f9fb6680e6bfd0632b953
mvill <119754846+migavg@users.noreply.github.com>
2023-03-08 16:53:38 -0700

    started with some code in the employee test file

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,2 +1,12 @@
 const Employee = require("../lib/Employee");
 
+describe("Employee", () => {
+    it("should  create a new employee", () => {
+       
+        const employee = new Employee("Miguel",93,"miguel@email.com");
+
+        expect();
+
+        
+    });
+})
